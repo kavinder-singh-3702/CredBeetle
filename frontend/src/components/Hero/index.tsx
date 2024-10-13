467ae8ce7d64b2262cdd397903a8f4ae5ba0e5b4
Kavinder Singh <kavinder.singh.3702@gmail.com>
2024-10-13 13:42:57 +0530

    HERO section

---
+++ frontend/src/components/Hero/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Left from "./Left";
+import Right from "./Right";
+
+export default function index() {
+  return (
+    <div>
+      <Left />
+      <Right />
+    </div>
+  );
+}

7cb24a5057ae5549c216acb3e0391c8858a858dc
Kavinder Singh <kavinder.singh.3702@gmail.com>
2024-12-11 21:55:44 +0530

    vercel

--- frontend/src/components/Hero/Left.jsx
+++ frontend/src/components/Hero/Left.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { SparklesCore } from "@/components/ui/Sparkles";
+import { SparklesCore } from "../ui/Sparkles";
 
 export default function Left() {
   return (

3bcba77c961dc4f6e37e1fd488a5f320b59c49f9
Kavinder Singh <kavinder.singh.3702@gmail.com>
2024-12-11 21:48:51 +0530

    removed scroll

--- frontend/src/components/Scroll/index.jsx
+++ frontend/src/components/Scroll/index.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import content from "@/data/scroll.tsx";
-import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
+// import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
 
 export default function index() {
   return (
@@ -9,7 +9,7 @@ export default function index() {
       <h2 className="text-7xl font-extrabold text-white text-center">
         Discover
       </h2>
-      <StickyScroll content={content} />
+      {/* <StickyScroll content={content} /> */}
     </section>
   );
 }

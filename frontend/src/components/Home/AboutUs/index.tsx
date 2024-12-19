1c2a868b3ad283104bfb7c4f4aced66fb517e1b1
Kavinder Singh <kavinder.singh.3702@gmail.com>
2024-12-19 23:00:48 +0530

    about us apge

---
+++ frontend/src/components/Home/AboutUs/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="text-white h-screen bg-red-50 text-lg max-w-4xl mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">About Us</h1>
+      <p className="leading-relaxed">
+        At Credbeetle, we're all about working coupons—no expired codes, no fake
+        deals, and absolutely no "Oops, this doesn't apply here" nonsense.
+        Everything you see here is 100% legit because we're the watchdogs of
+        savings who double-check, triple-check, and even quadruple-check every
+        deal before it gets to you.
+      </p>
+      <p className="leading-relaxed mt-4">
+        Didn't know you could save on that fancy coffee machine, your streaming
+        bill, or even your pet's spa day? Now you do, thanks to us! We're
+        treasure hunters for deals, turning your savings into pure fun. No
+        wasted time, no useless codes—just straight-up legit coupons that work
+        every time.
+      </p>
+      <p className="leading-relaxed mt-4">
+        Think of us as your coupon sidekicks, saving the world one valid deal at
+        a time because, hey, your savings deserve to work as hard as you do!
+      </p>
+    </div>
+  );
+}

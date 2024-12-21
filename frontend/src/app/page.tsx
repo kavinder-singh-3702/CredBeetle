9fb82bb7a2effba9e10d890b98c8a2db1ff2a06d
Shashi Kumar Gupta <shashigupta5909656@gmail.com>
2024-12-22 00:14:39 +0530

    Why Choose Us

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,11 +4,13 @@ import Statistics from "@/components/Statistics";
 import Footer from "@/components/Footer";
 import Sponsors from "@/components/Sponsors";
 import AboutUs from "@/components/Home/AboutUs";
+import WhyUs from "@/components/Home/WhyUs";
 export default function Home() {
   return (
     <section className="bg-black min-h-screen">
       <Hero />
       <AboutUs />
+      <WhyUs />
       <Scroll />
       <Statistics />
       <Sponsors />
